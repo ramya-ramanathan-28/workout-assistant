36037c8de65809303007fea1976404c7f0633d26
John Doe <johndoe@example.com>
2020-11-03 13:43:33 +0530

    Changes for auth

--- server/server.ts
+++ server/server.ts
@@ -1,20 +1,32 @@
-import bodyParser from "body-parser";
-import express from "express";
-import { postMessages, putMessage } from "./routes/messages";
-import { getUser } from "./routes/users";
+import { config } from 'dotenv';
+config();
+
+import cors from 'cors';
+
+import bodyParser from 'body-parser';
+import express from 'express';
+import { postMessages, putMessage } from './routes/messages';
+
+import { isLoggedIn, login, redirectCallback } from './routes/login';
+import { getSongs } from './routes/users';
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(cors());
 // user
-app.get("/api/users/:id", getUser);
+app.get('/api/songs', getSongs);
 
 // messages
-app.post("/api/messages", postMessages);
-app.put("/api/messages/:id", putMessage);
+app.post('/api/messages', postMessages);
+app.put('/api/messages/:id', putMessage);
 
 // tslint:disable-next-line:no-console
 app.listen(port, () => console.log(`Listening on port ${port}`));
+
+app.get('/login', login);
+
+app.get('/redirectCallback', redirectCallback);
+app.get('/isLoggedin', isLoggedIn);
