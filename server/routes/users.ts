4ab25e8c564cc54f9619a08978cfef339f9c1516
Ramya Ramanathan <raraman@microsoft.com>
2020-11-03 15:52:08 +0530

    Adding clustering of songs

--- server/routes/users.ts
+++ server/routes/users.ts
@@ -1,6 +1,9 @@
+import { createIntersectionTypeNode } from 'typescript';
 import { getToken } from '../data_music/config';
 import { httpget } from '../utils/httpfetch';
 const ObjectsToCsv = require('objects-to-csv');
+const kmeans = require('node-kmeans');
+
 
 const MYSONGS = 'https://api.spotify.com/v1/me/tracks';
 
@@ -19,16 +22,31 @@ export const getSongs = async (req: any, res: any) => {
     if(songs.length > 0){
       const songLink = `https://api.spotify.com/v1/audio-features/?ids=${ids}`
       let featuresList: Array<any> = []
+      let songIdList: Array<string> = []
       const songFeatures = await httpget(songLink, {
         headers: { Authorization: token },
       })
       songFeatures.data.audio_features.forEach(songFeature => {
         const {id, danceability, energy, loudness, speechiness, acousticness, instrumentalness, liveness, valence,tempo, duration_ms, time_signature} = songFeature;
-        featuresList.push({id, danceability, energy, loudness, speechiness, acousticness, instrumentalness, liveness, valence,tempo, duration_ms, time_signature});
+        featuresList.push([tempo, danceability, energy, loudness, speechiness, acousticness, instrumentalness, liveness, valence, duration_ms, time_signature]);
+        songIdList.push(id)
       });
-      const csv = new ObjectsToCsv(featuresList);
-      await csv.toDisk('./features.csv');
-      // To print: console.log(await csv.toString());
+      kmeans.clusterize(featuresList, {k: 3}, (err,res) => {
+        if (err) console.error(err);
+        else {
+        const clusterList: Array<{songs: Array<string>, intensity?: number}> = []
+        const centroid: Array<{tempo: number, centroid: number}> = []
+        res.forEach((cluster: any, index: number) => {
+          centroid.push({tempo: cluster.centroid[0], centroid: index})
+          clusterList.push({songs: cluster.clusterInd.map((index: number)=>songIdList[index])})
+        });
+        centroid.sort((a, b) => (a.tempo > b.tempo) ? 1 : -1)
+        centroid.forEach((val: any, index: number)=>{
+          clusterList[val.centroid]["intensity"] = index; 
+        })
+      }
+      // clusterList contains songs and their intensities(0: Low, 1: Medium, 2: High) 
+      });
     }
   } catch (err) {
     console.log(err);
