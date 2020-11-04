52b80c680a4dddf02d3145a98ac51388fbedb3f9
John Doe <johndoe@example.com>
2020-11-04 14:59:28 +0530

    context

--- src/components/graphs/LandingPage.tsx
+++ src/components/graphs/LandingPage.tsx
@@ -1,26 +1,29 @@
 import { Button } from 'office-ui-fabric-react';
 import React from 'react';
+import { LoginContext } from '../../contexts';
 
 export default function LandingPage(props: {
-  userName: React.ReactNode;
-  isLoggedIn: boolean;
   setStage: (stage: number) => void;
 }) {
   return (
-    <div className="landing-page-cont">
-      <div className="landing-page-greeting">
-        <strong>
-          Hi
-          <br></br>
-          {props.userName}
-        </strong>
-        <br></br>
-      </div>
-      <div className="landing-page-message">
-        {props.isLoggedIn
-          ? 'Lets Get Your Workout Started'
-          : 'Before Anything Lets Login'}
-      </div>
-    </div>
+    <LoginContext.Consumer>
+      {({ isLoggedin, userName }) => (
+        <div className="landing-page-cont">
+          <div className="landing-page-greeting">
+            <strong>
+              Hi
+              <br></br>
+              {userName}
+            </strong>
+            <br></br>
+          </div>
+          <div className="landing-page-message">
+            {isLoggedin
+              ? 'Lets Get Your Workout Started'
+              : 'Before Anything Lets Login'}
+          </div>
+        </div>
+      )}
+    </LoginContext.Consumer>
   );
 }
--- src/components/graphs/graph.tsx
+++ src/components/graphs/graph.tsx
@@ -1,17 +1,25 @@
 import React from 'react';
-import { graphClassNames } from './graph.styles'
+import { graphClassNames } from './graph.styles';
 
-function Graph() {
+function Graph(props: any) {
   return (
     <div className={graphClassNames.container}>
-    <div className={graphClassNames.row}>
-        <a className={graphClassNames.item}><img className={graphClassNames.graphImage} height="300px" src="./images/1.png"/></a>
-        <a className={graphClassNames.item}><img className={graphClassNames.graphImage} src="./images/2.png"/></a>
-    </div>
-    <div className={graphClassNames.row}>
-        <a className={graphClassNames.item}><img className={graphClassNames.graphImage} src="./images/3.png"/></a>
-        <a className={graphClassNames.item}><img className={graphClassNames.graphImage} src="./images/4.png"/></a>
-    </div>
+      <div className={graphClassNames.row}>
+        <a className={graphClassNames.item}>
+          <img className={graphClassNames.graphImage} src="./images/1.png" />
+        </a>
+        <a className={graphClassNames.item} onClick={() => {}}>
+          <img className={graphClassNames.graphImage} src="./images/2.png" />
+        </a>
+      </div>
+      <div className={graphClassNames.row}>
+        <a className={graphClassNames.item}>
+          <img className={graphClassNames.graphImage} src="./images/3.png" />
+        </a>
+        <a className={graphClassNames.item}>
+          <img className={graphClassNames.graphImage} src="./images/4.png" />
+        </a>
+      </div>
     </div>
   );
 }
