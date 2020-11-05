bd81eec96e02cf6728d7d97dfe430d18c37b26cc
Gopal Goel <gogoe@microsoft.com>
2020-11-05 14:26:02 +0530

    add workout code context

--- src/Body/mainBody.tsx
+++ src/Body/mainBody.tsx
@@ -1,66 +1,9 @@
-import { Button, mergeStyleSets } from 'office-ui-fabric-react';
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext} from 'react';
 import { CreateGraph } from '../components/graphs/CreateGraph';
 import LandingPage from '../components/graphs/LandingPage';
-import { SelectionPage } from '../components/graphs/SelectionPage';
 import { STAGES } from '../constant';
 import { AppStageContext } from '../contexts';
-const classes = mergeStyleSets({
-  divb: {
-    background: '#FFFFFF',
-  },
-});
-
-function SongList(props: any) {
-  const isloggedin = props.isLoggedIn;
-  const [showSongs, setShowSongs] = useState(false);
-  const [songsList, setSongsList] = useState([]);
-  useEffect(() => {
-    if (showSongs) {
-      fetch('http://localhost:5000/api/songs').then((response) =>
-        response.json().then((res) => setSongsList(res))
-      );
-    }
-  }, [showSongs]);
-  //if (isloggedin) {
-  const listItems1 = (songsList.filter((item: any) => {
-    return item.intensity == 0;
-  })[0] as any)?.songs.map((eachItem: any) => <li>{eachItem.name}</li>);
-
-  const listItems2 = (songsList.filter((item: any) => {
-    return item.intensity == 1;
-  })[0] as any)?.songs.map((eachItem: any) => <li>{eachItem.name}</li>);
-
-  const listItems3 = (songsList.filter((item: any) => {
-    return item.intensity == 2;
-  })[0] as any)?.songs.map((eachItem: any) => <li>{eachItem.name}</li>);
-
-  const ShowSongs = showSongs ? (
-    <div className={classes.divb}>
-      <text> Low</text>
-      <ul>{listItems1}</ul>
-      <text> Medium</text>
-      <ul>{listItems2}</ul>
-      <text> High</text>
-      <ul>{listItems3}</ul>
-    </div>
-  ) : null;
-  return (
-    <div>
-      <Button
-        onClick={() => {
-          setShowSongs(!showSongs);
-        }}
-      >
-        ShowSongs
-      </Button>
-      {ShowSongs}
-    </div>
-  );
-  //} else {
-  //return <Button disabled={!props.isloggedin}>ShowSongs</Button>;
-  // }
-}
+import SongList from "../components/graphs/SongsList"
 
 export default function MainBody(props: {}) {
   const { currentStage, setNextStage } = useContext(AppStageContext);
