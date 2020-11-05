35e838cf8863c00f7929c308c80c1186d294cdef
Gopal Goel <gogoe@microsoft.com>
2020-11-05 19:33:14 +0530

    integrate create playlist

--- src/components/graphs/SongsList.tsx
+++ src/components/graphs/SongsList.tsx
@@ -3,11 +3,24 @@ import { Button, DetailsList, mergeStyleSets } from 'office-ui-fabric-react';
 import {WorkoutContext} from "../../contexts";
 import  {DetailsListDocumentsExample} from "./Songs";
 
-// const classes = mergeStyleSets({
-//     divb: {
-//       background: '#FFFFFF',
-//     },
-//   });
+async function createPlaylist(url = "", data = {}) {
+  const baseurl = "http://localhost:5000/api/addSongsToPlaylist";
+  // Default options are marked with *
+  const response = await fetch(`${baseurl}${url}`, {
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    mode: 'cors', // no-cors, *cors, same-origin
+    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: 'same-origin', // include, *same-origin, omit
+    headers: {
+      'Content-Type': 'application/json'
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: 'follow', // manual, *follow, error
+    referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    body: JSON.stringify(data) // body data type must match "Content-Type" header
+  });
+  return response.json(); // parses JSON response into native JavaScript objects
+}
 
 function SongList(props: any) {
     const isloggedin = props.isLoggedIn;
@@ -65,6 +78,13 @@ function SongList(props: any) {
         <Button
           onClick={() => {
             // setShowSongs(!showSongs);
+            const songids = songsList.map((song: any) => {
+              return song.id;
+            })
+
+            fetch(`http://localhost:5000/api/addSongsToPlaylist?songIds=${songids.join()}`, { method:'post'}).then((response) =>
+              response.json().then((res) => console.log(res))
+            );
           }}
         >
           Create Playlist
@@ -78,3 +98,6 @@ function SongList(props: any) {
   }
 
   export default SongList;
+
+  // /{url: "https://open.spotify.com/playlist/1Zy1cej6y1EMiP0czag8kN"}
+// url: "https://open.spotify.com/playlist/1Zy1cej6y1EMiP0czag8kN"
